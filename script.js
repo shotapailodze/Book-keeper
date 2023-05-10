1491d581706ac6f21cfa93d14b083e4750288f74
Shota Pailodze <55694002+shotapailodze@users.noreply.github.com>
2023-05-10 22:50:28 +0400

    url validate

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+function validate(nameValue, urlValue) {
+    const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    const regex = new RegExp(expression);
+    if(!nameValue || !urlValue) {
+        alert('Please Submit Values For Both Fields')
+        return false
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please Provide A Valid Web Address');
+        return false;
+    }
+    // Valid
+    return true;
+}
+
+// Handle Data From Form
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+     urlValue = `https://${urlValue}`; 
+    }
+    console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+}
+
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark)
